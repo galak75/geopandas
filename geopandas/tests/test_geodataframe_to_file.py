3eef69f080523e1f4b0bb96386a68abe36feec38
Géraud <galak75@gmail.com>
2018-12-16 11:37:29 -0500

    just print exception_info objects

--- geopandas/tests/test_geodataframe_to_file.py
+++ geopandas/tests/test_geodataframe_to_file.py
@@ -201,16 +201,6 @@ class TestGeoDataFrameToFile():
             with pytest.raises(Exception) as exception_info:
                 mixed_geom_gdf.to_file(self.output_file, driver=ogr_driver)
             print("*** Exception_info: {}".format(exception_info))
-            assert (
-                (exception_info.type == RuntimeError and
-                 "Failed to write record" in str(exception_info)) or
-                (exception_info.type == ValueError and
-                 "Record's geometry type does not match collection schema's " +
-                 "geometry type" in str(exception_info)) or
-                (exception_info.type == AttributeError and
-                 "'list' object has no attribute 'lstrip'" in
-                 str(exception_info))
-            )
 
         else:
             self.do_test_geodataframe_to_file(mixed_geom_gdf, ogr_driver)
